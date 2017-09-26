8d850ed981bfd24dbbb9d9b933dd6fdf6c5cef7a
joderrick1022 <joderrick1022>
2017-09-26 14:19:49 -0400

    practiced with data types

--- week1_day2/Program.cs
+++ week1_day2/Program.cs
@@ -10,6 +10,21 @@ namespace week1_day2
     {
         static void Main(string[] args)
         {
+            //what data types have we talked about so far!
+            char firstInitial = 'o';
+                //characters or chars are sinle or numbers or symbols
+
+            bool isTired = true;
+            //boolean or bool is true or false.
+
+            //other number Data types
+            float number = 2.4647547382466f;
+            decimal partialNum = 2.18383774744774m;
+            double someNumber = 2.123444455;
+
+            Console.WriteLine(number);
+            Console.WriteLine(partialNum);
+            Console.WriteLine(someNumber);
         }
     }
 }
